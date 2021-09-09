458183481dea3007e9d295630eb2e71b417ddf1d
Victor Kotlin <grey.cat1908@gmail.com>
2021-09-09 17:24:41 +0300

    change on_connect to on_ready

--- examples/postdata.py
+++ examples/postdata.py
@@ -7,7 +7,7 @@ boticord = BoticordClient(bot, "your-boticord-token")
 
 
 @bot.event
-async def on_connect():
+async def on_ready():
     stats = {"servers": len(bot.guilds), "shards": bot.shard_count, "users": len(bot.users)}
     await boticord.Bots.postStats(stats)
 

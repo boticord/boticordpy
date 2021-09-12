c27521de8b781b9ade634f791c546b22e9ad8136
Victor Kotlin <grey.cat1908@gmail.com>
2021-09-12 12:58:17 +0300

    change versions

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text(encoding="utf8")
 setup(
     name="boticordpy",
     packages = ['boticordpy', 'boticordpy.modules'],
-    version="1.3.7",
+    version="1.4",
     description="Simple Python Module for boticord api",
     long_description=README,
     long_description_content_type="text/markdown",

f428c655196cb1729aad48f9d7aefdfe33bb6390
Victor Kotlin <grey.cat1908@gmail.com>
2021-09-14 20:18:03 +0300

    update docs theme again

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ release = '1.4'
 # ones.
 extensions = [
     'sphinx.ext.napoleon',
-    'sphinx.ext.ifconfig'
+    'sphinx.ext.ifconfig',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'groundwork'
+html_theme = "sphinx_rtd_theme"
 html_show_sourcelink = False
 
 # Add any paths that contain custom static files (such as style sheets) here,

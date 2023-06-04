241158b87a1215a7e8f16cb55864c0b613eb6147
MadCat9958 <abolvah8@gmail.com>
2023-06-04 19:04:51 +0300

    year change

--- boticordpy/__init__.py
+++ boticordpy/__init__.py
@@ -2,14 +2,14 @@
 Boticord API Wrapper
 ~~~~~~~~~~~~~~~~~~~
 A basic wrapper for the BotiCord API.
-:copyright: (c) 2022 Marakarka
+:copyright: (c) 2023 Marakarka
 :license: MIT, see LICENSE for more details.
 """
 
 __title__ = "boticordpy"
 __author__ = "Marakarka"
 __license__ = "MIT"
-__copyright__ = "Copyright 2022 Marakarka"
+__copyright__ = "Copyright 2023 Marakarka"
 __version__ = "2.2.2"
 
 from .client import BoticordClient

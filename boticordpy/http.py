737b77a99b90f6e82b15c2136df525a82d5f7909
MadCat9958 <abolvah8@gmail.com>
2023-07-03 19:58:25 +0300

    hotfix

--- boticordpy/http.py
+++ boticordpy/http.py
@@ -22,7 +22,7 @@ class HttpClient:
 
     def __init__(self, auth_token: str = None, version: int = 3, **kwargs):
         self.token = auth_token
-        self.API_URL = f"https://api.boticord.top/v{version}"
+        self.API_URL = f"https://api.boticord.top/v{version}/"
 
         loop = kwargs.get("loop") or asyncio.get_event_loop()
 

ea2bd433513d394ec5d837fe33842d841ed385cd
grey-cat-1908 <grey.cat1908@gmail.com>
2023-08-07 15:20:01 +0300

    3.0.2 -> 3.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = "2021 - 2023, Viktor K (Marakarka)"
 author = "Viktor K (Marakarka)"
 
 # The full version, including alpha/beta/rc tags
-release = "3.0.2"
+release = "3.0.3"
 
 
 # -- General configuration ---------------------------------------------------
